db0b12828f24ca4e153f434c2b6f97362a2282d9
IgorLutoshkin <ig.yarij@mail.ru>
2023-07-28 20:54:16 +0300

    home 2.js

---
+++ homework/1week/2.js
@@ -0,0 +1,9 @@
+/* При помощи сокращенной арифметики получи  total равный 195:  */
+
+
+let count1 = 10;
+let count2 = 20;
+let total = 0;
+
+total = (count1 + 3) * (count2 - 5);
+console.log(total);
