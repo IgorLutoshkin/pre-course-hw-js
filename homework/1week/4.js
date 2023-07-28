e17cbe018f3f3dd273b9121ce37e5a4b6a5eabcb
IgorLutoshkin <ig.yarij@mail.ru>
2023-07-28 22:08:38 +0300

    'home1-4'

---
+++ homework/1week/4.js
@@ -0,0 +1,9 @@
+/* Задание 1  на проверку знаний:
+Создай переменную myVariable и условие if else, по которому консолит: "Переменная содержит значение null" или "Переменная содержит [А СЮДА ВСТАВЬ ЗНАЧЕНИЕ myVariable ]"
+*/
+const myVariable = null;
+if (myVariable === null) {
+  console.log("Переменная содержит значение null");
+} else {
+  console.log("Переменная содержит " + myVariable);
+}
