6165575248320bfa297cc61767c242fa490604ce
IgorLutoshkin <ig.yarij@mail.ru>
2023-08-03 12:59:10 +0300

    'fix del comment in les3-01.js'

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,15 +1,3 @@
-/* У нас есть массив с цветами. При его помощи нам нужно вернуть (return) формулу флага Германии («черный-красный-желтый»). Для этого тебе понадобятся следующие методы: filter() и join(), а заодно вспомнить, что это такое ||- это пригодится для условия в методе filter()
-
-
-
-​const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
-
-function createColorString() {
-    return colors ФИЛЬТРУЕМ.ПРЕВРАЩАЕМ В СТРОКУ('-')
-}
-
-console.log(createColorString()); */
-
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
@@ -19,5 +7,4 @@ function createColorString() {
     )
     .join("-");
 }
-
 console.log(createColorString());
