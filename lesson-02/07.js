5b2045925f94edaa32c5dcdd2c07a1ef9081f9a4
IgorLutoshkin <ig.yarij@mail.ru>
2023-08-05 12:04:06 +0300

    fix les3-7

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -7,26 +7,7 @@ let passportMarried = {
   },
 };
 
-let passportMarried2 = {
-  ...passportMarried,
-  address: { ...passportMarried.address },
-};
-passportMarried2.married = true;
-
+let passportMarried2 = JSON.parse(JSON.stringify(passportMarried));
+passportMarried2.surname = { ...passportMarried.surname, married: true };
 console.log(passportMarried);
 console.log(passportMarried2);
-/* 
-// Проверяем, что только один парень женат
-let marriedCount = 0;
-
-if (passportMarried.married) {
-  marriedCount++;
-}
-
-if (passportMarried2.married) {
-  marriedCount++;
-}
-
-marriedCount === 1
-  ? "Все хорошо, только один парень женат"
-  : "Ошибка, несколько парней женаты"; */
