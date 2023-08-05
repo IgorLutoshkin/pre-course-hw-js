2454e6601932c370a46479e19e3b389727867ad7
IgorLutoshkin <ig.yarij@mail.ru>
2023-08-05 20:44:26 +0300

    fix le

---
+++ lesson-03/07..js
@@ -0,0 +1,12 @@
+/* const numbers = [
+  [1, 2, 3, 4, 5],
+  [11, 20, 33, 40, 55],
+  [111, 200, 333, 400, 555],
+];
+
+ */
+const tasks = [
+	{ id: 1, title: "HTML&CSS", isDone: true },
+	{ id: 2, title: "JS", isDone: true },
+	{ id: 3, title: "ReactJS", isDone: false }
+]
