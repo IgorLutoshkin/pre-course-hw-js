1a0c6417ea2dfee1f45b062e59b71107726da573
IgorLutoshkin <ig.yarij@mail.ru>
2023-08-11 15:19:02 +0300

    add homework les3/5

---
+++ homework/3week/5.js
@@ -0,0 +1,8 @@
+const tasks = [ 
+	{ id: 1, title: "HTML&CSS", isDone: true }, 
+	{ id: 2, title: "JS", isDone: true }, 
+	{ id: 3, title: "ReactJS", isDone: false } 
+] 
+console.log([...tasks,{id: 4, title: "Next", isDone: false }]);
+
+
